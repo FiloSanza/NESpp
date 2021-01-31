04bfdf985e7deb8089698aa35ce12eb435ce5fb4
FiloSanza <filippo.sanzani2001@gmail.com>
2021-01-31 17:43:29 +0100

    Project init

---
+++ cmake-build-debug/CMakeFiles/NESpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NESpp.dir/main.cpp.obj"
+  "NESpp.exe"
+  "NESpp.exe.manifest"
+  "NESpp.pdb"
+  "libNESpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NESpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
